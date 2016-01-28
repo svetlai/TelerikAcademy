1c4d5bf947c7f651eef919704ff797a3aff2ea9c
s i <svetla_22@yahoo.com>
2016-01-29 01:33:19 +0200

    Separate view for adding a gsm added plus helper methods.

---
+++ Software-Technologies/Mobile-Apps-for-iPhone-and-iPad/01-Objective-C-OOP/GSM/AddViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddViewController.h
+//  GSM
+//
+//  Created by s i on 1/29/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddViewController : UIViewController
+
+@end
